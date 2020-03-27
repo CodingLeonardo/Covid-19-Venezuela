5a88daaa0fbb95601a6396e2b3aa636741f18c72
Leonardo Rivero <leonardorivero145@gmail.com>
2020-03-27 13:10:25 -0400

    Agrego una interfaz de usuario muy simple

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import "./assets/css/styles.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>Covid-19 Venezuela</h1>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center shadow-2xl h-64 rounded">
+          <div className="flex items-center justify-center flex-col w-1/3 p-6 h-full">
+            <h1 className="text-2xl">Confirmados</h1>
+            <span>107</span>
+          </div>
+          <div className="flex items-center justify-center flex-col w-1/3 p-6 h-full">
+            <h1 className="text-2xl">Recuperados</h1>
+            <span>36</span>
+          </div>
+          <div className="flex items-center justify-center flex-col w-1/3 p-6 h-full">
+            <h1 className="text-2xl">Muertos</h1>
+            <span>1</span>
+          </div>
+        </div>
       </div>
     </>
   );
